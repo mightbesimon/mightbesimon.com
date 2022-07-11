ffc12915d570bb2d9f7c0a84626c4d031cfb8de7
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-07-12 00:01:44 +1200

    who are you?

---
+++ src/views/unauthenticated/AuthPage/AuthPage.tsx
@@ -0,0 +1,19 @@
+import SimonSvg from 'components/SimonSvg/SimonSvg';
+import Footer from 'components/Footer/Footer';
+
+function AuthPage() {
+	return (
+		<>
+			<header className='flex wrap'>
+				<SimonSvg />
+				<h1>WHO ARE YOU?</h1>
+			</header>
+			<section>
+
+			</section>
+			<Footer />
+		</>
+	)
+}
+
+export default AuthPage;
