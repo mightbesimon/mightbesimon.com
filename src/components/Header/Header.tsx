145613726ad7d16659ccbec5710f403fd20d0dcb
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-07-11 23:38:49 +1200

    add lights out mode to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import './Header.scss';
 import SimonSvg from 'components/SimonSvg/SimonSvg';
+import { useState } from 'react';
 
 function Header() {
+	const [lights, setLights] = useState(true);
+
 	return (
-		<header className='flex wrap'>
+		<header className={lights ? 'flex wrap' : 'flex wrap lights-out'}>
 			<SimonSvg />
-			<h1>SIMON</h1>
+			<h1
+				onMouseEnter={() => { setLights(false) }}
+				onMouseLeave={() => { setLights(true) }}
+			>SIMON</h1>
 		</header>
 	)
 }
