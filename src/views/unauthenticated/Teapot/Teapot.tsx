5f703529037f1145a09507ee80027c5f1e9e7794
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-10-15 13:07:34 +0200

    I'm a teapot

---
+++ src/views/unauthenticated/Teapot/Teapot.tsx
@@ -0,0 +1,11 @@
+import SectionWrapper from 'components/SectionWrapper/SectionWrapper'
+
+function NotFound() {
+	return (
+		<SectionWrapper view='Teapot' title='418' game>
+			<h1>I'M A TEAPOT 🫖</h1>
+		</SectionWrapper>
+	)
+}
+
+export default NotFound
