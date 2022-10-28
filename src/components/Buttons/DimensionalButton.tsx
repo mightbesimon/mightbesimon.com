69cf1f6120508894190d3eeaf0f1af439c577f79
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-10-28 23:47:37 +1300

    add properties to dimensional button

--- src/components/Buttons/DimensionalButton.tsx
+++ src/components/Buttons/DimensionalButton.tsx
@@ -2,13 +2,40 @@ import './DimensionalButton.scss';
 
 type DimensionalButtonProps = {
 	children?: JSX.Element | JSX.Element[] | string,
+	width?: string,
+	height?: string,
+	borderRadius?: {
+		base?: string,
+		button?: string,
+	},
+	colour?: {
+		base?: string,
+	},
+	font?: {
+		size?: string,
+		colour?: string,
+	},
 };
 
-function DimensionalButton({ children }: DimensionalButtonProps): JSX.Element {
+function DimensionalButton({ children,
+	width, height, borderRadius, colour, font
+}: DimensionalButtonProps): JSX.Element {
 	return (
-		<div className='button dimensional'>
-			<div>
-				<div>{children}</div>
+		<div className='button dimensional'
+			style={{
+				width, height,
+				borderRadius: borderRadius?.base,
+				background: colour?.base,
+			}}
+		>
+			<div style={{ borderRadius: borderRadius?.button, }}>
+				<div style={{
+					borderRadius: borderRadius?.button,
+					fontSize: font?.size,
+					color: font?.colour,
+				}}>
+					{children}
+				</div>
 			</div>
 		</div>
 	);
--- src/views/unauthenticated/Showcase/ButtonsPage/ButtonsPage.tsx
+++ src/views/unauthenticated/Showcase/ButtonsPage/ButtonsPage.tsx
@@ -1,6 +1,6 @@
 import './ButtonsPage.scss';
 import CoveredButton from 'components/Buttons/CoveredButton';
-// import DimensionalButton from 'components/Buttons/DimensionalButton';
+import DimensionalButton from 'components/Buttons/DimensionalButton';
 import SectionWrapper from 'components/SectionWrapper/SectionWrapper';
 
 function ButtonsPage(): JSX.Element {
@@ -8,9 +8,17 @@ function ButtonsPage(): JSX.Element {
 		<SectionWrapper view='ButtonsPage'>
 			<div>
 				<CoveredButton>Don't</CoveredButton>
-				{/* <DimensionalButton>?</DimensionalButton> */}
+				<CoveredButton>Don't</CoveredButton>
+				<DimensionalButton
+					width='3.5rem' height='3.2rem'
+					borderRadius={{ base: '10%', button: '50%' }}
+					font={{ size: '.8rem' }}
+				>
+					?
+				</DimensionalButton>
+				<DimensionalButton>?</DimensionalButton>
 			</div>
-		</SectionWrapper>
+		</SectionWrapper >
 	);
 }
 
