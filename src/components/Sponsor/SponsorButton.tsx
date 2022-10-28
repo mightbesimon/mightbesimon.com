35936f2d805d955b25d615c34bf672b8f1493f3e
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-10-28 20:11:57 +1300

    change to 3d sponsor button

--- src/components/Sponsor/SponsorButton.tsx
+++ src/components/Sponsor/SponsorButton.tsx
@@ -1,10 +1,22 @@
 import './SponsorButton.scss';
 
+// function SponsorButton(): JSX.Element {
+// 	return (
+// 		<a className='sponsor button' href='https://github.com/sponsors/mightbesimon'>
+// 			♥️ <span>Sponsor</span>
+// 		</a>
+// 	);
+// }
+
 function SponsorButton(): JSX.Element {
 	return (
-		<a className='sponsor button' href='https://github.com/sponsors/mightbesimon'>
-			♥️ <span>Sponsor</span>
-		</a>
+		<div className='sponsor button'>
+			<a href='https://github.com/sponsors/mightbesimon'>
+				<div>
+					♥️ <span>Sponsor</span>
+				</div>
+			</a>
+		</div>
 	);
 }
 
