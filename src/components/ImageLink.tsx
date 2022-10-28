2d282b6d3590114e99c4d353081961bb4809ff06
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-10-29 02:09:17 +1300

    allow open in new tab

--- src/components/ImageLink.tsx
+++ src/components/ImageLink.tsx
@@ -2,11 +2,15 @@ type ImageLinkProps = {
 	name: string,
 	image: string,
 	url: string,
+	newTab?: boolean,
 };
 
-function ImageLink({ name, image, url }: ImageLinkProps) {
+function ImageLink({ name, image, url, newTab }: ImageLinkProps) {
 	return (
-		<a className='image link' key={name} href={url}>
+		<a className='image link' key={name}
+			href={url} target={newTab ? '_blank' : undefined}
+			rel='noopener noreferrer'
+		>
 			<img alt={name} src={image} />
 		</a>
 	)
--- src/components/SvgLink.tsx
+++ src/components/SvgLink.tsx
@@ -1,12 +1,16 @@
-type SvgLinkProps = {
+export type SvgLinkProps = {
 	name: string,
 	svg: React.FunctionComponent<React.SVGProps<SVGSVGElement>>,
 	url: string,
+	newTab?: boolean,
 };
 
-function SvgLink({ name, svg: Svg, url }: SvgLinkProps) {
+function SvgLink({ name, svg: Svg, url, newTab }: SvgLinkProps) {
 	return (
-		<a className='svg image link' key={name} href={url}>
+		<a className='svg image link' key={name}
+			href={url} target={newTab ? '_blank' : undefined}
+			rel='noopener noreferrer'
+		>
 			<Svg />
 		</a>
 	)
