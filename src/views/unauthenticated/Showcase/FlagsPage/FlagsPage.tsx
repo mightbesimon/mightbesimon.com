87afb4b2fb1bc39ef4dd29b4870118b4ec934729
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-11-28 01:53:56 +1300

    test pypi stats api allow origin

--- src/views/unauthenticated/Showcase/FlagsPage/FlagsPage.tsx
+++ src/views/unauthenticated/Showcase/FlagsPage/FlagsPage.tsx
@@ -1,8 +1,17 @@
 import './FlagsPage.scss';
 import { FrenchFlag, GermanFlag, IrishFlag, ItalianFlag, USFlag } from 'components/Flags/Flags';
 import SectionWrapper from 'components/SectionWrapper/SectionWrapper';
+import getPackageDownloads from 'utils/api/pypi/getPackageDownloads';
+import { useQuery } from 'react-query';
 
-function FlagsPage(): JSX.Element {
+function FlagsPage(): JSX.Element
+{
+	const metaclassesDownloads = useQuery(
+		'getPackageDownloads',
+		() => getPackageDownloads({ package: 'lifehacks.metaclasses' }),
+		{ staleTime: 300000 }
+	);
+
 	return (
 		<SectionWrapper view='FlagsPage'>
 			<div>
@@ -15,6 +24,14 @@ function FlagsPage(): JSX.Element {
 			</div>
 			<div>
 				<USFlag />
+			</div>
+			<div>
+				{
+					metaclassesDownloads.data &&
+					'lifehacks.metaclasses: '
+					+ metaclassesDownloads.data
+						.reduce((sum, data) => sum + data.downloads, 0)
+				}
 			</div>
 		</SectionWrapper>
 	);
