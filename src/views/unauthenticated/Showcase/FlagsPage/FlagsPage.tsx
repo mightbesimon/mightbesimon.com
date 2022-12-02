1b41149cf1d7a8226054626075c6aad49161e1c2
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-12-03 00:24:34 +1300

    remove testing query call

--- src/views/unauthenticated/Showcase/FlagsPage/FlagsPage.tsx
+++ src/views/unauthenticated/Showcase/FlagsPage/FlagsPage.tsx
@@ -1,17 +1,9 @@
 import './FlagsPage.scss';
 import { FrenchFlag, GermanFlag, IrishFlag, ItalianFlag, USFlag } from 'components/Flags/Flags';
 import SectionWrapper from 'components/SectionWrapper/SectionWrapper';
-import getPackageDownloads from 'utils/api/pypi/getPackageDownloads';
-import { useQuery } from 'react-query';
 
 function FlagsPage(): JSX.Element
 {
-	const metaclassesDownloads = useQuery(
-		'getPackageDownloads',
-		() => getPackageDownloads({ package: 'lifehacks.metaclasses' }),
-		{ staleTime: 300000 }
-	);
-
 	return (
 		<SectionWrapper view='FlagsPage'>
 			<div>
@@ -24,14 +16,6 @@ function FlagsPage(): JSX.Element
 			</div>
 			<div>
 				<USFlag />
-			</div>
-			<div>
-				{
-					metaclassesDownloads.data &&
-					'lifehacks.metaclasses: '
-					+ metaclassesDownloads.data
-						.reduce((sum, data) => sum + data.downloads, 0)
-				}
 			</div>
 		</SectionWrapper>
 	);
