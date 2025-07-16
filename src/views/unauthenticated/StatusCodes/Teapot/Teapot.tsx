05673b330ed42aa4b43d00cefdfcdb69a7d1951e
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2025-07-16 14:56:56 +0200

    update teapot to new style

--- src/views/unauthenticated/StatusCodes/Teapot/Teapot.tsx
+++ src/views/unauthenticated/StatusCodes/Teapot/Teapot.tsx
@@ -1,10 +1,24 @@
+import DimensionalButton from 'components/Buttons/DimensionalButton';
 import SectionWrapper from 'components/SectionWrapper/SectionWrapper';
+import url from 'data/links.json';
 
 function Teapot()
 {
 	return (
 		<SectionWrapper view='Teapot' title='418' game>
-			<h1>I'M A TEAPOT 🫖</h1>
+			<h1>I'M A TEAPOT <span className='stroke'>🫖</span></h1>
+			<p>Can't make coffee, but you can!</p>
+			<a className='sponsor button'
+				href={url.sponsor}
+				target='_blank' rel='noopener noreferrer'
+				style={{ margin: '.25rem auto' }}
+			>
+				<DimensionalButton width='12.5rem' height='2rem'>
+					<span className='stroke'>👉</span>
+					<span style={{ margin: '0 .5rem' }}>Shout me a coffee</span>
+					<span className='stroke'>👈</span>
+				</DimensionalButton>
+			</a>
 		</SectionWrapper>
 	);
 }
