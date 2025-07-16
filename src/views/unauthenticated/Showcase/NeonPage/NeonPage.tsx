3ba41d68de98c3ad11739f11a31e1bcd5c4b985f
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2025-07-16 10:10:32 +0200

    update neon page and remove bag of cans studio

--- src/views/unauthenticated/Showcase/NeonPage/NeonPage.tsx
+++ src/views/unauthenticated/Showcase/NeonPage/NeonPage.tsx
@@ -1,20 +1,12 @@
 import './NeonPage.scss';
-import { ReactComponent as LogoOff } from 'assets/BagOfCansStudio/logo-off.svg';
 import SectionWrapper from "components/SectionWrapper/SectionWrapper";
 
 function NeonPage()
 {
 	return (
 		<SectionWrapper view='NeonPage'>
-			<div className='neon'>
-				<p className='large'>NEON</p>
-				<p>BAG <span>OF</span> CAN<span>S</span></p>
-				<p>STUDIO</p>
-			</div>
-			<div className='BagOfCans flex column'>
-				<LogoOff />
-				<LogoOff className='two' />
-				<LogoOff className='three' />
+			<div className='neon flex'>
+				<div>NEON</div>
 			</div>
 		</SectionWrapper>
 	);
